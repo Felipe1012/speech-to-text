ca80731ad64a8265b2c9defb9a6b68623dd0cf89
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-09-25 13:48:50 -0500

    v1

---
+++ routes/router.js
@@ -0,0 +1,34 @@
+const { Router } = require("express")
+const stt = require("./utils/stt");
+const params = require("../params");
+const router = Router();
+
+
+router.post('/stt', async function (req,  res) {
+
+    // res.send("hey world")
+    const input = req.body.audio;
+    console.log(input);
+    try {
+        if (!input) {
+            res.send({
+                status: false,
+                message: "No file uploaded",
+            })
+        } else {
+            let finalJson = [];
+            await stt(params, input).then((ans) => {
+                finalJson.push(ans)
+                res.send(ans)
+            }
+            );
+
+        }
+    } catch (err) {
+        res.status(500).json({ message: "No se pudo analizar el archivo ingresado" });
+    }
+});
+
+
+
+module.exports = router;    
---
+++ routes/utils/stt.js
@@ -0,0 +1,55 @@
+
+/**
+  *
+  * @param {object} params
+  * @param {string} params.iam_apikey
+  * @param {string} params.url
+  * @param {string} params.input
+  * @return {object}
+  *
+  */
+
+var SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
+var fs = require('fs');
+const { resolve } = require('path');
+
+
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+/**
+* Processing Json result of Watson Natural Language Understanding process
+* @param {JSON} resultNL - respuesta del watson NL en JSON
+*/
+
+function stt(params, input){
+    return new Promise(function (resolve, reject) {
+
+var speechToText = new SpeechToTextV1({
+    iam_apikey: params.iam_apikey,
+    url: params.url
+  });
+    
+  speechToText.recognize({
+    'audio':fs.createReadStream(input),
+    'content_type': params.content_type,
+    'model': params.model
+  }, function(err, res) {
+    if (err){
+      return resolve(err);
+  
+    }
+    res=JSON.stringify(res,null,2)
+    console.log(res)
+      return resolve(res)
+  });
+});
+
+}
+module.exports=stt;
