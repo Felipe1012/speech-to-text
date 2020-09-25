68e08957244596d009c17a3587ada8a078ca01e3
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-09-25 18:28:10 -0500

    v3

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app= express();
 const morgan = require('morgan');
 
 app.set('port', process.env.PORT || 3000);
+var favicon = require('serve-favicon')
+var path = require('path')
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
  //routes
 app.use(require('./routes/router'))
+app.use(favicon(path.join(__dirname, 'public', '../imagen.png')))
 
 
 app.listen(app.get('port'),() =>{
