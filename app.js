c8b74a973b6d6ada766544504d85bf6c22f2ecd7
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-09-28 09:32:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const morgan = require('morgan');
 const cors = require("cors");
 
 app.set('port', process.env.PORT || 3000);
-var favicon = require('serve-favicon')
 var path = require('path')
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
  //routes
 app.use(require('./routes/router'))
-app.use(favicon(path.join( 'public', '../imagen.png')))
 app.use(cors());
+
+app.use(express.static(__dirname + "build")); //
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "../imagen.png")); // <- try "index.html"
+});
 
 
 app.listen(app.get('port'),() =>{
