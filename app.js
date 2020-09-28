a71c08a5267c73cdb4dfa035f4940eaeca1ebc8a
Felipe1012 <60628168+Felipe1012@users.noreply.github.com>
2020-09-27 19:01:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
  //routes
-app.use(require('./routes/router'))
-app.use(favicon(path.join(__dirname, 'public', '../imagen.png')))
+//app.use(require('./routes/router'))
+//app.use(favicon(path.join(__dirname, 'public', '../imagen.png')))
 
 
 app.listen(app.get('port'),() =>{
